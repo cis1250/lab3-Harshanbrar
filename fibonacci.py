cd47be92c360c111e7935cf203d36696a5e037cb
Harshan Brar <harshan@uoguelph.ca>
2025-09-29 11:59:03 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 
 # Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+
+while True:
+    print("How many sequences would you like?")
+    try:
+        sequences = int(input())   # convert input to integer
+        print(f'\nYou chose {sequences} sequences')
+
+        if sequences == 0:
+            print(0)
+
+        elif sequences > 0:
+            a, b = 0, 1
+            print("Fibonacci sequence:")
+            print(a)
+            if sequences > 1:
+                print(b)
+            for i in range(2, sequences):
+                next_term = a + b
+                print(next_term)
+                a = b
+                b = next_term
+
+        else:
+            print("\nPlease input a positive number.")
+
+    except ValueError:
+        print("\nInvalid input. Please enter a positive integer.")
